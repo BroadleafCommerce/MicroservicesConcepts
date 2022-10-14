6a6778d70f219c796372f93a0d635db2c68d2825
jefffischer <jfischer@broadleafcommerce.org>
2022-10-14 13:45:45 -0500

    BroadleafCommerce/MicroPM#3263 - Cleanup. Enhance for admin metadata for extensions.

--- concepts/productExtensionOnly/reset-and-run.sh
+++ concepts/productExtensionOnly/reset-and-run.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+function exitfn (){
+    echo "Early Termination"
+    exit
+}
+
+trap "exitfn" INT
 rm -rf ./src/main/resources/db
 rm -rf ./src/main/resources/application*
 printf "\n"
 printf "***** Building App And Generating Schema *****"
 printf "\n"
-mvn clean install -Pschema
+cd ../../
+mvn clean install
 printf "\n"
 printf "***** Launching Supporting Components *****"
 printf "\n"
-cd ../../docker
+cd docker
 docker-compose down
 docker-compose pull
 docker-compose up -d
--- concepts/productExtensionOnly/src/main/java/com/tutorial/ProductExtensionOnly.java
+++ concepts/productExtensionOnly/src/main/java/com/tutorial/ProductExtensionOnly.java
@@ -1,28 +1,93 @@
 package com.tutorial;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.broadleafcommerce.catalog.metadata.autoconfigure.CatalogMetadataProperties;
+import com.broadleafcommerce.catalog.metadata.support.DefaultProductType;
+import com.broadleafcommerce.catalog.metadata.support.ProductGroups;
+import com.broadleafcommerce.catalog.metadata.support.ProductIds;
 import com.broadleafcommerce.catalog.provider.jpa.domain.product.JpaProduct;
 import com.broadleafcommerce.common.jpa.data.entity.JpaEntityScan;
+import com.broadleafcommerce.data.tracking.core.mapping.AutoConfigureProjectionMapping;
+import com.broadleafcommerce.metadata.dsl.core.extension.views.details.EntityView;
+import com.broadleafcommerce.metadata.dsl.core.utils.Fields;
+import com.broadleafcommerce.metadata.dsl.registry.ComponentSource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+import static com.broadleafcommerce.catalog.metadata.support.DefaultProductType.values;
 import static com.broadleafcommerce.catalog.provider.RouteConstants.Persistence.CATALOG_ROUTE_PACKAGE;
 
+/**
+ * Setup components for the extension case. Note, admin metadata service and resource tier service
+ * information are both contributed here. This works without further intervention for the tutorial
+ * use case because both the metadata and catalog services are contained in the same Min FlexPackage
+ * runtime used by the tutorial. In other FlexPackage combinations, where these two services are not
+ * co-located, the jar containing these components would need to be referenced as a dependency in
+ * the multiple relevant locations.
+ */
+@Configuration
 public class ProductExtensionOnly {
 
+    /**
+     * Spring configuration to wire the extension
+     */
     @Configuration
     @JpaEntityScan(basePackages = "com.tutorial", routePackage = CATALOG_ROUTE_PACKAGE)
     public static class Config {}
 
+    /**
+     * Spring configuration to establish admin metadata for the domain extension.
+     */
+    @Configuration
+    @ConditionalOnBean(CatalogMetadataProperties.class)
+    @RequiredArgsConstructor
+    public static class AdminConfig {
+
+        private final CatalogMetadataProperties properties;
+
+        @Bean
+        public ComponentSource myProductMetadataComponents() {
+            return registry -> {
+                List<DefaultProductType> types = Arrays.stream(values())
+                        .filter(t -> properties.getActiveProductTypes().contains(t.name()))
+                        .collect(Collectors.toList());
+                for (DefaultProductType type : types) {
+                    Arrays.asList(
+                            (EntityView<?>) registry
+                                    .get(String.format(ProductIds.CREATE, type.name())),
+                            (EntityView<?>) registry
+                                    .get(String.format(ProductIds.UPDATE, type.name())))
+                            .forEach(view -> view.getGeneralForm()
+                                    .getGroup(ProductGroups.BASIC_INFORMATION)
+                                    .addField(Fields.colorPicker()
+                                            .name("color")
+                                            .label("color")
+                                            .defaultValue("#ffffff")));
+                }
+            };
+        }
+
+    }
+
+    /**
+     * Simple column domain extension
+     */
     @Entity
-    @Table(name = "MY_PRODUCT_EXTENSION")
+    @Table(name = "TUTORIAL_PRODUCT")
     @Inheritance(strategy = InheritanceType.JOINED)
     @Data
-    public static class JpaProductExtension extends JpaProduct {
+    @AutoConfigureProjectionMapping
+    public static class TutorialProduct extends JpaProduct {
 
         @Column(name = "COLOR")
         private String color;
