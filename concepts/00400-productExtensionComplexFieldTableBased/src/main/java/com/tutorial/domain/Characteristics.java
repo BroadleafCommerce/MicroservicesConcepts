047fbdd45e6c58b3a1727f5b457c15182c9a8765
jefffischer <jfischer@jfischer.com>
2023-05-24 12:37:21 -0500

    BroadleafCommerce/MicroPM#3607 - Update example to prescribed pattern for OneToOne relationships

--- concepts/00400-productExtensionComplexFieldTableBased/src/main/java/com/tutorial/domain/Characteristics.java
+++ concepts/00400-productExtensionComplexFieldTableBased/src/main/java/com/tutorial/domain/Characteristics.java
@@ -1,7 +1,5 @@
 package com.tutorial.domain;
 
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
 import org.springframework.util.Assert;
 import com.broadleafcommerce.data.tracking.core.mapping.ProjectionPostConvert;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -9,11 +7,11 @@ import java.math.BigDecimal;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.EqualsAndHashCode;
@@ -31,14 +29,12 @@ import static com.broadleafcommerce.common.jpa.JpaConstants.CONTEXT_ID_LENGTH;
 public class Characteristics {
 
     @Id
-    @GeneratedValue(generator = "blcid")
-    @GenericGenerator(name = "blcid", strategy = "blcid")
-    @Type(type = "com.broadleafcommerce.data.tracking.jpa.hibernate.ULidType")
-    @Column(name = "ID", nullable = false, length = CONTEXT_ID_LENGTH)
+    @Column(name = "ELECTRIC_CAR_ID", nullable = false, length = CONTEXT_ID_LENGTH)
     private String id;
 
     @OneToOne(cascade = CascadeType.REFRESH)
     @JoinColumn(name = "ELECTRIC_CAR_ID")
+    @MapsId
     @ToString.Exclude // Avoid infinite recursion in toString()
     @JsonIgnore // Avoid infinite recursion in data tracking
     private MyAutoCoProduct car;
